cfeb17238cda11f24c18dea1ded41932b96953bc
troye <troyegilbert@pm.me>
2024-08-11 00:31:03 +0300

    apply migrations

---
+++ systemUI/ecommerceUI/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Product(models.Model):
+    category_1 = models.CharField(max_length=255)
+    category_2 = models.CharField(max_length=255)
+    category_3 = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    product_rating = models.FloatField()
+    selling_price = models.FloatField()
+    mrp = models.FloatField()
+    seller_name = models.CharField(max_length=255)
+    seller_rating = models.FloatField()
+    price_ratio = models.FloatField()
+    price_difference = models.FloatField()
+    product_id = models.IntegerField(unique=True)
+
+    def __str__(self):
+        return self.title
+
